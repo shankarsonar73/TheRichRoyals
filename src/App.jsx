705d92479cbdb9542b42a7ac3b6cf0573a4eea7d
shankarsonar123 <65942841+shankarsonar123@users.noreply.github.com>
2025-01-08 12:58:35 +0530

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Home from "./Pages/Home";
-// import AdminpanelFinal from "./Pages/AdminpanelFinal";
-// import EnhancedLoginPage from "./Pages/EnhancedLoginPage";
-// import { AuthProvider } from "./components/AuthContext";
-// import ProtectedRoute from "./components/ProtectedRoute";
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <Router>
-//         <Routes>
-//           <Route path="/" element={<Home/>} />
-//           <Route path="/login" element={<EnhancedLoginPage/>} />
-//           <Route path="/adminpanel" element={<ProtectedRoute element={<AdminpanelFinal/>} />} />
-//         </Routes>
-//       </Router>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
